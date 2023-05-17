b07edfd13909dacb36f536896b9bdca4080d7ac9
Tomasella Tobias <tobiastomasella@gmail.com>
2023-05-17 09:17:29 -0300

    Front-1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let username = '';
+    let password = '';
+  
+    function handleSubmit() {
+      alert(`Nombre de usuario: ${username}\nContraseña: ${password}`);
+    }
+  </script>
+  
+  <main>
+    <h1>Iniciar sesión</h1>
+  
+    <form on:submit|preventDefault={handleSubmit}>
+      <label>
+        Nombre de usuario:
+        <input type="text" bind:value={username} />
+      </label>
+  
+      <label>
+        Contraseña:
+        <input type="password" bind:value={password} />
+      </label>
+  
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  </main>
+  
+  <style>
+    main {
+      max-width: 400px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+  
+    h1 {
+      margin-bottom: 1rem;
+    }
+  
+    label {
+      display: block;
+      margin-bottom: 0.5rem;
+    }
+  
+    input {
+      width: 100%;
+      padding: 0.5rem;
+      font-size: 1rem;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+    }
+  
+    button {
+      margin-top: 1rem;
+      padding: 0.5rem 1rem;
+      font-size: 1rem;
+      border-radius: 4px;
+      background-color: #007bff;
+      color: #fff;
+      border: none;
+    }
+  
+    button:hover {
+      background-color: #0056b3;
+    }
+  </style>
+ 
+  
+  
